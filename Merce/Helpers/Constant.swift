5a3ae51ad481409850b803818ee9d05f0344ebf0
Mark <markrachapoom@gmail.com>
2022-11-17 15:19:43 +0800

    Change font size

--- Merce/Helpers/Constant.swift
+++ Merce/Helpers/Constant.swift
@@ -21,8 +21,8 @@ struct K {
     }
     
     static let rowPaddingVertical = 6.0
-    static let fontSize = 15.5
-    static let bottomTabBarHeight: CGFloat = 56.0
+    static let fontSize: CGFloat = 15.0
+    static let bottomTabBarHeight: CGFloat = 52.0
     static let coverImageHeight: CGFloat = 175
     
     // MARK: - HAPTIC FEEDBACK
